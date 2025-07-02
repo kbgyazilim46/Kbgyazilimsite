50481f57d3229864ad1d3ca8897b9d2a2ef3091c
kbgyazilim46 <kbgyazilim46@gmail.com>
2025-07-03 00:51:32 +0300

    app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def hakkinda():
 
     <hr>
     <h2>İletişim</h2>
-    <p><strong>Telefon:</strong> <a href='tel:wa.me://+639092363984'>wa.me;//+639092363984</a></p>
+    <p><strong>Telefon:</strong> <a href='tel:wa.me/+639092363984'>wa.me/+639092363984</a></p>
     <p><strong>Instagram:</strong> <a href='https://instagram.com/_kimsebasgoz_' target='_blank'>@_kimsebasgoz_</a></p>
     <p><strong>E-posta:</strong> <a href='mailto:kimsebasgozedemez@gmail.com'>kimsebasgozedemez@gmail.com</a></p>
     """
@@ -46,7 +46,7 @@ def hakkinda():
 def iletisim():
     content = """
     <h1>İletişim</h1>
-    <p><strong>Telefon:</strong> <a href='tel:wa.me://+639092363984'>wa.me://+639092363984</a></p>
+    <p><strong>Telefon:</strong> <a href='tel:wa.me/+639092363984'>wa.me/+639092363984</a></p>
     <p><strong>Instagram:</strong> <a href='https://instagram.com/_kimsebasgoz_' target='_blank'>@_kimsebasgoz_</a></p>
     <p><strong>E-posta:</strong> <a href='mailto:kimsebasgozedemez@gmail.com'>kimsebasgozedemez@gmail.com</a></p>
     """
